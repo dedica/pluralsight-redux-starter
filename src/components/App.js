057e0ae3a3c117f92580ecacf95e5865c784c045
dedica <dedica@algonquincollege.com>
2017-06-29 09:56:15 -0400

    static heading removed

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ class App extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-                <p>Header here ...</p>
                 {this.props.children}
             </div>
         );
