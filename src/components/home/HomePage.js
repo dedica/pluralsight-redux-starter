ae18ec18e6eb546854978927cbd585e56ddb7fda
dedica <dedica@algonquincollege.com>
2017-06-29 09:53:53 -0400

    fixed class to className

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router';
 class HomePage extends React.Component {
     render() {
         return (
-            <div class="jumbotron">
+            <div className="jumbotron">
                 <h1>Pluralsight Administration</h1>
                 <p>React, Redux and React-router in ES6 for ultra-responsive web-apps.</p>
                 <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
